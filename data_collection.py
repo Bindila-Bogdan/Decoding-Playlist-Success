46a8383d0a4a9161fc5871887c74a1dd66d5ef6d
sarang2551 <sarang.nirwan@gmail.com>
2024-01-19 08:47:54 +0100

    Added genre and taylor swift analysis

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, lower, explode, size, avg, expr
+from pyspark.sql.functions import col, count, lower, explode, size, avg, expr, array_contains, when
 
 sc = SparkContext(appName="webApp")
 sc.setLogLevel("ERROR")
@@ -9,7 +9,7 @@ sc.setLogLevel("ERROR")
 spark = SparkSession(sc).builder.getOrCreate()
 # Clean data: s3264440
 # Original data: s3264424
-playlists_data = spark.read.json("/user/s3264440/project_group_18/data/spotify_playlists/", multiLine=True)
+playlists_data = spark.read.json("/user/s3264424/project_group_18/data/spotify_playlists/", multiLine=True)
 audio_features = spark.read.json("/user/s3264440/project_group_18/data/audio_features/").distinct()
 artist_data = spark.read.json("/user/s3264440/project_group_18/data/artists/").distinct()
 
@@ -44,7 +44,7 @@ def get_aggregated_audio_features(playlists_data,audio_features,data_type='mean'
         joined_df = joined_df.groupBy('pid').agg(expr('percentile(num_followers, 0.5)').alias('num_followers'),expr('percentile(acousticness, 0.5)').alias('acousticness'),expr('percentile(danceability, 0.5)').alias('danceability'),expr('percentile(duration_ms, 0.5)').alias('duration_ms'),expr('percentile(energy, 0.5)').alias('energy'),expr('percentile(instrumentalness, 0.5)').alias('instrumentalness'),expr('percentile(key, 0.5)').alias('key'),expr('percentile(liveness, 0.5)').alias('liveness'),expr('percentile(loudness, 0.5)').alias('loudness'),expr('percentile(mode, 0.5)').alias('mode'),expr('percentile(speechiness, 0.5)').alias('speechiness'),expr('percentile(tempo, 0.5)').alias('tempo'),expr('percentile(valence, 0.5)').alias('valence'))
         aggregated_df = joined_df.groupBy('num_followers').agg(expr('percentile(acousticness, 0.5)').alias('acousticness'),expr('percentile(danceability, 0.5)').alias('danceability'),expr('percentile(duration_ms, 0.5)').alias('duration_ms'),expr('percentile(energy, 0.5)').alias('energy'),expr('percentile(instrumentalness, 0.5)').alias('instrumentalness'),expr('percentile(key, 0.5)').alias('key'),expr('percentile(liveness, 0.5)').alias('liveness'),expr('percentile(loudness, 0.5)').alias('loudness'),expr('percentile(mode, 0.5)').alias('mode'),expr('percentile(speechiness, 0.5)').alias('speechiness'),expr('percentile(tempo, 0.5)').alias('tempo'),expr('percentile(valence, 0.5)').alias('valence'))
     aggregated_df.show()
-    
+
     output_path = f"/user/s3307891/aggregated_audio_{data_type}"
     aggregated_df.write.mode("overwrite").option("header", "true").csv(output_path)
 
@@ -73,8 +73,13 @@ def genres_per_playlist(playlists_data,artist_data):
     followers_grouped.write.mode("overwrite").option("header", "true").csv(output_path)
 
 
-def taylor_swift_feature(playlist_data,artist_data):
-    pass
-
+def taylor_swift_feature(playlists_data,artist_data):
+    exploded_df = playlists_data.select(explode("playlists").alias("playlist"))
+    exploded_df = exploded_df.select("playlist.pid", "playlist.tracks.artist_name","playlist.num_followers")
+    #taylor_swift_df = exploded_df.withColumn("is_taylor_swift", when(array_contains(col("artist_name"), "Taylor Swift"), 1).otherwise(0))
+    taylor_swift_df = exploded_df.withColumn("taylor_swift_count", expr("size(filter(artist_name, x -> x == 'Taylor Swift'))"))
+    relevant_df = taylor_swift_df.groupBy("num_followers").agg(avg("taylor_swift_count").alias("avg_taylor_swift")).drop("pid").drop("artist_name")
+    output_path = "/user/s3307891/taylor_feature"
+    relevant_df.write.mode("overwrite").option("header", "true").csv(output_path)
 
 get_aggregated_audio_features(playlists_data,audio_features)
