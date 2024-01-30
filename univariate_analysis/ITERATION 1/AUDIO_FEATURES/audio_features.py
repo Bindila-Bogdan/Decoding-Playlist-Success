da51fc82361fc14b975152150c698df190f9c5a2
smriti199 <smriti007sgp@gmail.com>
2024-01-30 17:38:21 +0100

    added iteration 2 sans data

--- univariate_analysis/ITERATION 1/AUDIO_FEATURES/audio_features.py
+++ univariate_analysis/ITERATION 1/AUDIO_FEATURES/audio_features.py
@@ -13,19 +13,19 @@ audio_features_df = spark.read.json("/user/s3264440/project_group_18/data/audio_
 audio_features = audio_features_df.select("id", "acousticness", "danceability", "energy","instrumentalness", "liveness", "loudness", "speechiness","tempo", "valence")
 audio_features_summary = audio_features.describe()
 #audio_features_summary.show()
-path_audio_features_summary = "/user/s3307913/CLEAN/AUDIO/SONGLEVEL"
+path_audio_features_summary = "/user/s3307913/FINAL/AUDIO/SONGLEVEL"
 audio_features_summary.write.csv(path_audio_features_summary, header=True, mode='overwrite')
 
 # Analysis 2: Track Duration Analysis
 track_duration_stats = audio_features_df.describe("duration_ms")
 
-path_track_duration_stats = "/user/s3307913/CLEAN/AUDIO/TRACKDURATION"
+path_track_duration_stats = "/user/s3307913/FINAL/AUDIO/TRACKDURATION"
 track_duration_stats.write.csv(path_track_duration_stats, header=True,mode='overwrite')
 
 # Analysis 3: Mode of Tracks
 mode_distribution = audio_features_df.groupBy("mode").count()
 
-path_mode_distribution = "/user/s3307913/CLEAN/AUDIO/MODEDISTRIBUTION"
+path_mode_distribution = "/user/s3307913/FINAL/AUDIO/MODEDISTRIBUTION"
 mode_distribution.write.csv(path_mode_distribution, header=True, mode='overwrite')
 
 
--- univariate_analysis/ITERATION 1/PLAYLISTS/playlists.py
+++ univariate_analysis/ITERATION 1/PLAYLISTS/playlists.py
@@ -11,38 +11,38 @@ playlists_df = spark.read.json("/user/s3264440/project_group_18/data/spotify_pla
 # Analysis 1: Playlist Composition Aggregation
 playlist_composition_agg = playlists_df.agg(avg("num_artists").alias("avg_num_artists"),max("num_artists").alias("max_num_artists"),min("num_artists").alias("min_num_artists"),stddev("num_artists").alias("stddev_num_artists"),avg("num_albums").alias("avg_num_albums"),max("num_albums").alias("max_num_albums"),min("num_albums").alias("min_num_albums"),stddev("num_albums").alias("stddev_num_albums"),avg("num_tracks").alias("avg_num_tracks"),max("num_tracks").alias("max_num_tracks"),min("num_tracks").alias("min_num_tracks"),stddev("num_tracks").alias("stddev_num_tracks"))
 
-path_playlist_composition = "/user/s3307913/CLEAN/PLAYLISTS/COMPOSITION"
+path_playlist_composition = "/user/s3307913/FINAL/PLAYLISTS/COMPOSITION"
 playlist_composition_agg.write.csv(path_playlist_composition, header=True, mode='overwrite')
 
 # Analysis 2: Playlist Duration Aggregation
 playlist_duration_stats = playlists_df.describe("duration_ms")
 
-path_playlist_duration = "/user/s3307913/CLEAN/PLAYLISTS/PLAYLISTDURATION"
+path_playlist_duration = "/user/s3307913/FINAL/PLAYLISTS/PLAYLISTDURATION"
 playlist_duration_stats.write.csv(path_playlist_duration, header=True, mode='overwrite')
 
 # Analysis 3: Playlist Name Analysis
 word_counts = playlists_df.select(explode(split(col("name"), "\s+")).alias("word")).groupBy("word").count().orderBy("count", ascending=False)
 
-path_word_counts = "/user/s3307913/CLEAN/PLAYLISTS/PLAYLISTNAMEWC"
+path_word_counts = "/user/s3307913/FINAL/PLAYLISTS/PLAYLISTNAMEWC"
 word_counts.write.csv(path_word_counts, header=True, mode='overwrite')
 
 name_length = playlists_df.withColumn("name_length", length("name"))
 name_length_stats = name_length.describe("name_length")
 
-path_name_length = "/user/s3307913/CLEAN/PLAYLISTS/PLAYLISTNAMELENGTH"
+path_name_length = "/user/s3307913/FINAL/PLAYLISTS/PLAYLISTNAMELENGTH"
 name_length_stats.write.csv(path_name_length, header=True, mode='overwrite')
 
 # Analysis 4: Playlist Modification 
 modification_date_df = playlists_df.withColumn("modified_date", to_date(from_unixtime("modified_at")))
 modification_trends = modification_date_df.groupBy("modified_date").count().orderBy("modified_date")
 
-path_modification_trends = "/user/s3307913/CLEAN/PLAYLISTS/PLAYLISTMODITRENDS"
+path_modification_trends = "/user/s3307913/FINAL/PLAYLISTS/PLAYLISTMODITRENDS"
 modification_trends.write.csv(path_modification_trends, header=True, mode='overwrite')
 
 # Analysis 5: Follower Count Statitics
 follower_counts_stats = playlists_df.describe("num_followers")
 
-path_follower_stats = "/user/s3307913/CLEAN/PLAYLISTS/PLAYLISTFOLLOWERSTATS"
+path_follower_stats = "/user/s3307913/FINAL/PLAYLISTS/PLAYLISTFOLLOWERSTATS"
 follower_counts_stats.write.csv(path_follower_stats, header=True, mode='overwrite')
 
 # Analysis 6: Playlist Descriptions  -- DIDNT END UP USING YET
@@ -51,17 +51,17 @@ words_in_description = playlists_df.withColumn("word", explode(split(col("descri
 # Counting word frequency
 word_counts = words_in_description.groupBy("word").count().orderBy("count", ascending=False)
 
-path_word_counts = "/user/s3307913/CLEAN/PLAYLISTS/PLAYLISTNAMEDESC"
+path_word_counts = "/user/s3307913/FINAL/PLAYLISTS/PLAYLISTNAMEDESC"
 word_counts.write.csv(path_word_counts, header=True, mode='overwrite')
 
 # Analysis 7: Edit Stats
 edit_counts_stats = playlists_df.describe("num_edits")
 
-path_edit_counts_stats = "/user/s3307913/CLEAN/PLAYLISTS/PLAYLISTEDITS"
+path_edit_counts_stats = "/user/s3307913/FINAL/PLAYLISTS/PLAYLISTEDITS"
 edit_counts_stats.write.csv(path_edit_counts_stats, header=True, mode='overwrite')
 
 # Analysis 8: Collaborative Counts
 collaborative_counts = playlists_df.groupBy("collaborative").count()
 
-path_collaborative_counts = "/user/s3307913/CLEAN/PLAYLISTS/PLAYLISTTYPE"
+path_collaborative_counts = "/user/s3307913/FINAL/PLAYLISTS/PLAYLISTTYPE"
 collaborative_counts.write.csv(path_collaborative_counts, header=True, mode='overwrite')
